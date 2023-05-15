2d2ccab878ab34dbf127645d3c85a7d1283e93fd
Imani 'K!ΠG' Niyigena <imanix8@gmail.com>
2023-05-15 12:15:35 +0200

    CMAKE foundation establisment with C++

---
+++ SvelteSupaRise/SvelteSupaRise.cpp
@@ -0,0 +1,12 @@
+﻿// SvelteSupaRise.cpp : Defines the entry point for the application.
+//
+
+#include "SvelteSupaRise.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
